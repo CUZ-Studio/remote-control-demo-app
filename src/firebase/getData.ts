22dda97b028520bc082896836d58d99682fd6eab
dahyeko <dalgona92@gmail.com>
2023-06-12 17:39:03 +0900

    feat: connect app to firebase db

---
+++ src/firebase/getData.ts
@@ -0,0 +1,18 @@
+import { doc, getDoc } from "firebase/firestore";
+
+import { db } from "./config";
+
+export default async function getDoument(collection: string, id: string) {
+  const docRef = doc(db, collection, id);
+
+  let result = null;
+  let error = null;
+
+  try {
+    result = await getDoc(docRef);
+  } catch (e) {
+    error = e;
+  }
+
+  return { result, error };
+}
