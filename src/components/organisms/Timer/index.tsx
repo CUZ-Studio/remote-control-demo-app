f4b70be65ae9eab3bf599e1c606c7714ca775699
dahyeko <dahye.ko@cuz-art.com>
2023-06-20 12:58:29 +0900

    docs: add comments

--- src/components/organisms/Timer/index.tsx
+++ src/components/organisms/Timer/index.tsx
@@ -25,13 +25,15 @@ export default function Timer() {
 
   useEffect(() => {
     if (minutes + seconds <= 0) {
-      if (!player.model || !player.color || !player.headTag) return;
-
+      // 0초 남은 시점에 진행중인 게임은 없는 것으로 전역 상태 업데이트
       assignPlayer({
         ...player,
         isGameInProgress: false,
       });
+
+      // 대략 10초 (지난 라운드 결과 보여주는 시간) 있다가,
       setTimeout(() => {
+        // 기존에 생성한 캐릭터 그대로 생성
         axios
           .put(`${process.env.NEXT_PUBLIC_UNREAL_DOMAIN}/remote/object/call`, {
             objectPath: gameRound.gameModeBaseObjectPath,
@@ -45,23 +47,25 @@ export default function Timer() {
             generateTransaction: true,
           })
           .then((res) => {
+            // 캐릭터 생성 성공시 오브젝트 상대 경로 업데이트
             assignPlayer({
               ...player,
               objectPath: res.data.CharacterPath,
             });
-            // 캐릭터가 게임 화면 중심에 나타나게 하기
+            // 생성된 캐릭터가 게임 화면 중심에 나타나게 하기
             axios.put(`${process.env.NEXT_PUBLIC_UNREAL_DOMAIN}/remote/object/call`, {
               objectPath: res.data.CharacterPath,
               functionName: "SetPlayerLocation",
               generateTransaction: true,
             });
+            // 게임 남은 시간, 게임진행여부 상태 업데이트
             updateGameRound({
               ...gameRound,
               isGameInProgress: true,
               timeLeft: res.data.MainGameRemainTime,
             });
           });
-      }, 2000);
+      }, 10500);
     }
   }, [minutes, seconds]);
 
--- src/pages/play.tsx
+++ src/pages/play.tsx
@@ -2,18 +2,27 @@ import Image from "next/image";
 
 import GameStatusBar from "@/components/molecules/GameStatusBar";
 import ControlPanel from "@/components/organisms/ControlPanel";
+import useGameStatus from "@/hooks/useGameRound";
 import usePlayer from "@/hooks/usePlayer";
 
 import { Container, PlayerInfoBox, PlayerName, Score } from "@/styles/play.styles";
 
 export default function PlayGame() {
+  const { isGameInProgress } = useGameStatus();
   const player = usePlayer();
+
   return (
     <Container>
       <PlayerInfoBox>
-        <Image src="/assets/images/star.svg" alt="별" width={17} height={17} />
-        <PlayerName>{player.headTag || "ddsf"}</PlayerName>
-        <Score>{player.score || 4560}</Score>
+        {isGameInProgress ? (
+          <>
+            <Image src="/assets/images/star.svg" alt="reward" width={17} height={17} />
+            <PlayerName>{player.headTag || "ddsf"}</PlayerName>
+            <Score>{player.score || 4560}</Score>
+          </>
+        ) : (
+          <div>dfdf</div>
+        )}
       </PlayerInfoBox>
       <ControlPanel />
       <GameStatusBar />
